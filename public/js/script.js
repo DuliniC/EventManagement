5fe5ea6d4e976088df4ff1639464ec247b36d4e4
DuliniC <dchamathka9@gmail.com>
2024-09-11 22:02:03 +0530

    eventview

--- public/js/script.js
+++ public/js/script.js
@@ -23,6 +23,10 @@ document.addEventListener("DOMContentLoaded", function () {
   var selects = document.querySelectorAll("select");
   M.FormSelect.init(selects);
 
+  var elems = document.querySelectorAll('.modal');
+  M.Modal.init(elems);
+
+
   // Open side panel
   addCategoryBtn.addEventListener("click", () => {
     sidePanelC.style.width = "25%";
@@ -75,53 +79,54 @@ document.addEventListener("DOMContentLoaded", function () {
     eventLocation = {};
   });
 
-  const scrollContainer = document.getElementById('scroll-container');
+  //category filter move
+  const scrollContainer = document.getElementById("scroll-container");
   let isDragging = false;
   let startX;
   let scrollLeft;
 
-  scrollContainer.addEventListener('mousedown', (e) => {
-      isDragging = true;
-      startX = e.pageX - scrollContainer.offsetLeft;
-      scrollLeft = scrollContainer.scrollLeft;
-      scrollContainer.style.cursor = 'grabbing';
+  scrollContainer.addEventListener("mousedown", (e) => {
+    isDragging = true;
+    startX = e.pageX - scrollContainer.offsetLeft;
+    scrollLeft = scrollContainer.scrollLeft;
+    scrollContainer.style.cursor = "grabbing";
   });
 
-  scrollContainer.addEventListener('mouseleave', () => {
-      isDragging = false;
-      scrollContainer.style.cursor = 'grab';
+  scrollContainer.addEventListener("mouseleave", () => {
+    isDragging = false;
+    scrollContainer.style.cursor = "grab";
   });
 
-  scrollContainer.addEventListener('mouseup', () => {
-      isDragging = false;
-      scrollContainer.style.cursor = 'grab';
+  scrollContainer.addEventListener("mouseup", () => {
+    isDragging = false;
+    scrollContainer.style.cursor = "grab";
   });
 
-  scrollContainer.addEventListener('mousemove', (e) => {
-      if (!isDragging) return;
-      e.preventDefault();
-      const x = e.pageX - scrollContainer.offsetLeft;
-      const walk = (x - startX) * 2; // Adjust scroll speed
-      scrollContainer.scrollLeft = scrollLeft - walk;
+  scrollContainer.addEventListener("mousemove", (e) => {
+    if (!isDragging) return;
+    e.preventDefault();
+    const x = e.pageX - scrollContainer.offsetLeft;
+    const walk = (x - startX) * 2; // Adjust scroll speed
+    scrollContainer.scrollLeft = scrollLeft - walk;
   });
 
-  scrollContainer.addEventListener('touchstart', (e) => {
-      isDragging = true;
-      startX = e.touches[0].pageX - scrollContainer.offsetLeft;
-      scrollLeft = scrollContainer.scrollLeft;
-      scrollContainer.style.cursor = 'grabbing';
+  scrollContainer.addEventListener("touchstart", (e) => {
+    isDragging = true;
+    startX = e.touches[0].pageX - scrollContainer.offsetLeft;
+    scrollLeft = scrollContainer.scrollLeft;
+    scrollContainer.style.cursor = "grabbing";
   });
 
-  scrollContainer.addEventListener('touchend', () => {
-      isDragging = false;
-      scrollContainer.style.cursor = 'grab';
+  scrollContainer.addEventListener("touchend", () => {
+    isDragging = false;
+    scrollContainer.style.cursor = "grab";
   });
 
-  scrollContainer.addEventListener('touchmove', (e) => {
-      if (!isDragging) return;
-      const x = e.touches[0].pageX - scrollContainer.offsetLeft;
-      const walk = (x - startX) * 2; // Adjust scroll speed
-      scrollContainer.scrollLeft = scrollLeft - walk;
+  scrollContainer.addEventListener("touchmove", (e) => {
+    if (!isDragging) return;
+    const x = e.touches[0].pageX - scrollContainer.offsetLeft;
+    const walk = (x - startX) * 2; // Adjust scroll speed
+    scrollContainer.scrollLeft = scrollLeft - walk;
   });
 
   //Event Banner Image Preview
@@ -144,17 +149,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
   //Submit Event Form
   eventAddForm.addEventListener("submit", function (event) {
-    var location = document.getElementById('event-location');
+    var location = document.getElementById("event-location");
 
     event.preventDefault();
-    const data = {
+
+    const data =
+      // prettier-ignore
+      {
       "name": document.getElementById("event-name").value,
       "details": document.getElementById("details").value,
       "date": new Date(document.getElementById("date").value),
       "location": {
         "address": location.value,
-        "latitude":Number(location.getAttribute('latitude')),
-        "longitude":Number(location.getAttribute('longitude'))
+        "latitude": Number(location.getAttribute("latitude")),
+        "longitude": Number(location.getAttribute("longitude")),
       },
       "timeStart": document.getElementById("timeStart").value,
       "timeEnd": document.getElementById("timeEnd").value,
@@ -200,7 +208,7 @@ async function fetchCategories() {
   }
 }
 
-function showCategories(categories){
+function showCategories(categories) {
   const categoryList = document.getElementById("categoryList");
   categoryList.innerHTML = "";
 
@@ -216,12 +224,12 @@ function showCategories(categories){
     tr.innerHTML = `
       <td id="category-name-${category._id}">${category.name}</td>
       <td>
-          <button class="btn-small btn-floating light-blue accent-3" id="editBtn" onclick="updateCategory('${category._id}')">
+          <button class="btn-medium btn-floating light-blue accent-3" id="editBtn" onclick="updateCategory('${category._id}')">
             <i class="material-icons">edit</i>
           </button>
       </td>
       <td>
-          <button class="btn-small red btn-floating" id="deleteBtn" onclick="deleteCategory('${category._id}')">
+          <button class="btn-medium red btn-floating" id="deleteBtn" onclick="deleteCategory('${category._id}')">
             <i class="material-icons">delete</i>
           </button>
       </td>
@@ -234,22 +242,28 @@ function showCategories(categories){
   categoryList.appendChild(table);
 }
 
-function showCategoryFilters(categories){
-  const cardList = document.getElementById('scroll-container');
-  cardList.innerHTML = '';
+function showCategoryFilters(categories) {
+  const cardList = document.getElementById("scroll-container");
+  cardList.innerHTML = "";
 
   // Create and append the "All" category button
   const allCard = document.createElement("div");
-  allCard.classList.add("option-card", "center-align", "white", "z-depth-1", "active"); // Make "All" active by default
+  allCard.classList.add(
+    "option-card",
+    "center-align",
+    "white",
+    "z-depth-1",
+    "active"
+  ); // Make "All" active by default
 
-  allCard.innerHTML = `<p>All</p>`;
+  allCard.innerHTML = `<h5>All</h5>`;
 
   // Add event listener for the "All" category
-  allCard.addEventListener("click", function() {
-    document.querySelectorAll('.option-card').forEach(card => {
-      card.classList.remove('active');
+  allCard.addEventListener("click", function () {
+    document.querySelectorAll(".option-card").forEach((card) => {
+      card.classList.remove("active");
     });
-    allCard.classList.add('active');
+    allCard.classList.add("active");
     getEvents();
   });
 
@@ -259,18 +273,18 @@ function showCategoryFilters(categories){
     const card = document.createElement("div");
     card.classList.add("option-card", "center-align", "white", "z-depth-1");
     card.innerHTML = `
-    <p>${category.name}</p>`;
+    <h5>${category.name}</h5>`;
 
-    card.addEventListener("click", function() {
-      document.querySelectorAll('.option-card').forEach(card => {
-        card.classList.remove('active');
+    card.addEventListener("click", function () {
+      document.querySelectorAll(".option-card").forEach((card) => {
+        card.classList.remove("active");
       });
 
-      card.classList.add('active');
+      card.classList.add("active");
       getEventsByCategory(category._id);
     });
     cardList.appendChild(card);
-  })
+  });
 }
 
 //GET All Events
@@ -280,31 +294,31 @@ async function getEvents() {
     await fetch("/events")
       .then((response) => response.json())
       .then((events) => {
-        setEventCardContainer(events)
+        setEventCardContainer(events);
       });
   } catch (e) {
     console.log(e);
   }
 }
 
-async function getEventsByCategory(category){
-  try{
+async function getEventsByCategory(category) {
+  try {
     showSpinner();
     await fetch(`/events/category/${category}`)
       .then((response) => response.json())
       .then((events) => {
-        setEventCardContainer(events)
+        setEventCardContainer(events);
       });
-  } catch(e){
+  } catch (e) {
     console.log(e);
   }
 }
 
 let markers = [];
 
-function setEventCardContainer(events){
+function setEventCardContainer(events) {
   const cardContainer = document.getElementById("event-container");
-  cardContainer.innerHTML = '';
+  cardContainer.innerHTML = "";
   if (events.length === 0) {
     const noDataMessage = document.createElement("div");
     noDataMessage.classList.add("no-data");
@@ -314,7 +328,7 @@ function setEventCardContainer(events){
     hideSpinner();
     return;
   }
-  markers.forEach(marker => {
+  markers.forEach((marker) => {
     marker.setMap(null); // Removes the marker from the map
   });
   markers = [];
@@ -329,9 +343,8 @@ function setEventCardContainer(events){
           event.banner
         }" onerror="this.onerror=null; this.src='./img/img-noload.jpg';" alt="Event Image">
         <span class="card-title">
-          ${new Date(event.date).toDateString()} <br/> ${
-      event.timeStart
-    } - ${event.timeEnd}
+          ${new Date(event.date).toDateString()} <br/> 
+          ${event.timeStart} - ${event.timeEnd}
         </span>
       </div>
       <div class="card-content">
@@ -342,22 +355,29 @@ function setEventCardContainer(events){
         <div class="row">
           <div class="col s1"><i class="material-icons icon-location">location_on</i></div>
           <div class="col s5">${event.location?.address}</div>
-          <div class="col s6">
+          <div class="col s5">
             <button class="waves-light btn" id="rsvp-btn-${event._id}" 
-                onclick="updateRSVP('${event._id}',${event.attendees})">RSVP</button>
+                onclick="updateRSVP('${event._id}',${
+      event.attendees
+    })">RSVP</button>
             <span id="attendees-count-${event._id}">${
       event.attendees
     } Going</span>
           </div>
+          <div class="col s1">
+          <button class="btn-flat view-event-btn"><i class="material-icons icon-view">visibility</i></button></div>
         </div>
-    </div>
+      </div>
     `;
     cardContainer.appendChild(card);
+
+    const viewButton = card.querySelector('.view-event-btn');
+    viewButton.addEventListener('click', () => viewEvent(event));
 
     var marker = new google.maps.Marker({
       position: { lat: event.location.latitude, lng: event.location.longitude },
       map: map,
-      title: event.address
+      title: event.address,
     });
 
     markers.push(marker);
@@ -366,10 +386,10 @@ function setEventCardContainer(events){
       content: `<div><strong>${event.name}</strong><br>
       ${new Date(event.date).toDateString()}<br>
       ${event.timeStart} - ${event.timeEnd}<br>
-      ${event.location.address}</div>`
+      ${event.location.address}</div>`,
     });
 
-    marker.addListener('click', function() {
+    marker.addListener("click", function () {
       infoWindow.open(map, marker);
     });
 
@@ -377,8 +397,40 @@ function setEventCardContainer(events){
   });
 }
 
+function viewEvent(event) {
+  const modal = document.getElementById("eventModal");
+    const modalTitle = document.getElementById("modal-title");
+    const modalContent = document.getElementById("modal-content");
+
+    // Populate the modal with event details
+    modalTitle.innerHTML = event.name;
+    modalContent.innerHTML = `
+    <div class="row">
+     <div class="col s12 m6 l6">
+      <img id="modal-image" src="data:image/png;base64,${event.banner}" alt="Event Image" class="responsive-img modal-image">
+     </div>
+     <div class="col s12 m6 l6">
+      <p id="modal-date-time" class="modal-info">
+      <i class="material-icons">event</i><b>Date:</b> ${new Date(event.date).toDateString()}</p>
+      <p id="modal-date-time" class="modal-info">
+      <i class="material-icons">timelapse</i><b>Time:</b> ${event.timeStart} - ${event.timeEnd}</p>
+      <p id="modal-location" class="modal-info">
+      <i class="material-icons">location_city</i><b>Location:</b> ${event.location?.address}</p>
+      <p id="modal-organizer" class="modal-info">
+      <i class="material-icons">assignment_ind</i><b>Organizer:</b>${event.organizerName} - ${event.organizerContact}</p>
+      <p id="modal-location" class="modal-info">
+      <i class="material-icons">location_city</i>${event.attendees} Going to Attend</p>
+      <p id="modal-details" class="modal-info">${event.details}</p>
+     </div>
+    </div>
+    `;
+
+    // Open the modal
+    const modalInstance = M.Modal.getInstance(modal); // Assuming you're using Materialize
+    modalInstance.open();
+}
+
 function populateSelect(data) {
-  //var selects = document.querySelectorAll("select");
   var select = document.getElementById("selectCategory");
 
   var instance = M.FormSelect.getInstance(select);
@@ -392,7 +444,7 @@ function populateSelect(data) {
     option.value = item._id;
     option.textContent = item.name;
     option.index;
-    select.appendChild(option); // Append option to the select element
+    select.appendChild(option); 
   });
 
   M.FormSelect.init(select);
@@ -418,65 +470,73 @@ function updateRSVP(id) {
   }
 }
 
-function deleteCategory(id){
-  try{
+function deleteCategory(id) {
+  try {
     fetch(`/categories/${id}`, {
-      method: "DELETE"
+      method: "DELETE",
     });
     M.toast({ html: "Category Deleted" });
     const row = document.getElementById(`category-row-${id}`);
     if (row) {
       row.remove();
     }
-  }catch (err){
-    console.log()
+  } catch (err) {
+    console.log();
   }
 }
 
-function updateCategory(id){
-  document.querySelectorAll('button').forEach(button => button.disabled = true);
+function updateCategory(id) {
+  document
+    .querySelectorAll("button")
+    .forEach((button) => (button.disabled = true));
 
   const cell = document.getElementById(`category-name-${id}`);
   var categoryValue = cell.textContent;
 
-  cell.innerHTML =` <input type="text" id="edit-category-input-${id}" value="${categoryValue}">
+  cell.innerHTML = ` <input type="text" id="edit-category-input-${id}" value="${categoryValue}">
         <button class="btn-small light-blue accent-3 waves-effect waves-light btn" onClick="saveUpdateCategory('${id}')">Save</button>
         <button class="btn-small red waves-effect waves-light btn" onclick="cancelUpdateCategory('${id}','${categoryValue}')">Cancel</button>`;
-
 }
 
-function cancelUpdateCategory(id, value){
-  document.querySelectorAll('button').forEach(button => button.disabled = false);
+function cancelUpdateCategory(id, value) {
+  document
+    .querySelectorAll("button")
+    .forEach((button) => (button.disabled = false));
   const cell = document.getElementById(`category-name-${id}`);
   cell.textContent = value;
 }
 
-function saveUpdateCategory(id){
-  document.querySelectorAll('button').forEach(button => button.disabled = false);
+function saveUpdateCategory(id) {
+  document
+    .querySelectorAll("button")
+    .forEach((button) => (button.disabled = false));
   const newValue = document.getElementById(`edit-category-input-${id}`).value;
   const cell = document.getElementById(`category-name-${id}`);
   cell.textContent = newValue;
 
-  try{
+  try {
     fetch(`/categories/${id}`, {
       method: "PUT",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ name: newValue })
+      body: JSON.stringify(
+        // prettier-ignore
+        { "name": newValue }
+      ),
     });
     M.toast({ html: "Category Updated" });
-  }catch (err){
-    console.log()
+  } catch (err) {
+    console.log();
   }
 }
 
 function showSpinner() {
-  document.getElementById('spinner').style.display = 'block';
+  document.getElementById("spinner").style.display = "block";
 }
 
 function hideSpinner() {
-  document.getElementById('spinner').style.display = 'none';
+  document.getElementById("spinner").style.display = "none";
 }
 
 let map;
@@ -487,29 +547,28 @@ async function initMap() {
 
   map = new Map(document.getElementById("map"), {
     center: { lat: -25.2744, lng: 133.7751 },
-    zoom: 4.5
+    zoom: 4.5,
   });
 }
 
-async function initMapPlace(){
+async function initMapPlace() {
   //@ts-ignore
   await google.maps.importLibrary("places");
   const location = document.getElementById("event-location");
   //@ts-ignore
-  placeAutocomplete = new google.maps.places.Autocomplete(location,{
-    componentRestrictions: {country: 'au'},
+  placeAutocomplete = new google.maps.places.Autocomplete(location, {
+    componentRestrictions: { country: "au" },
   });
-  
+
   //@ts-ignore
-  placeAutocomplete.addListener('place_changed', function() {
+  placeAutocomplete.addListener("place_changed", function () {
     var place = placeAutocomplete.getPlace();
     if (!place.geometry || !place.geometry.location) {
-        console.log("No details available for input: '" + place.name + "'");
-        return;
-    }   
+      console.log("No details available for input: '" + place.name + "'");
+      return;
+    }
 
-    location.setAttribute('latitude',place.geometry.location.lat());
-    location.setAttribute('longitude',place.geometry.location.lng());
+    location.setAttribute("latitude", place.geometry.location.lat());
+    location.setAttribute("longitude", place.geometry.location.lng());
   });
-
 }
